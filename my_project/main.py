960ab50c2c76fb93cb1e5f7d3b74cff34a2cb6ad
kavindumal <kavindu11250403@gmail.com>
2024-11-28 16:43:39 +0530

    Refactor main.py to import specific functions from addition and subtraction modules

--- my_project/main.py
+++ my_project/main.py
@@ -1,5 +1,26 @@
-import my_calculator.addition as add
-import my_calculator.substraction as sub
+# Step 01
+# import my_calculator.addition
+# import my_calculator.substraction
+
+# result1 = my_calculator.addition.add(55 , 65)
+# result2 = my_calculator.substraction.sub(75 , 65)
+
+# print(result1)
+# print(result2)
+
+# Step 02
+# from my_calculator import addition
+# from my_calculator import substraction
+
+# result1 = addition.add(55 , 65)
+# result2 = substraction.sub(75 , 65)
+
+# print(result1)
+# print(result2)
+
+# Step 03
+from my_calculator.addition import add
+from my_calculator.substraction import sub
 
 result1 = add.add(55 , 65)
 result2 = sub.sub(75 , 65)
