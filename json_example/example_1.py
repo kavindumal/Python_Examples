36fa4bd81d0d932c88f72b18a0d659307880d23e
kavindumal <kavindu11250403@gmail.com>
2024-12-12 12:07:09 +0530

    Add example JSON data generation and create my_json.json file

--- json_example/example_1.py
+++ json_example/example_1.py
@@ -1,7 +1,20 @@
 import json
 
-json_file_path = 'json_example/example_1.json'
+# json_file_path = 'json_example/example_1.json'
 
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    print(data, type(data))
+# with open(json_file_path, 'r') as file:
+#     data = json.load(file)
+#     for key, value in data.items():
+#         print(f"{key}: {value}")
+
+data = {
+    'name': 'John Doe',
+    'age' : 30,
+    'city' : 'New York',
+}
+
+with open('json_example/my_json.json', 'w') as file:
+    json_data = json.dumps(data, indent=4)
+    file.write(json_data)
+    
+# use indent = 4 to make the json data more readable with spacing
